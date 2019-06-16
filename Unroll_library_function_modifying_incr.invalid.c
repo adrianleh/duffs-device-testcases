f6880cf161ce2458785525a47213cdd177b35117
Adrian Lehmann <info@adrianlehmann.net>
2019-06-16 17:20:09 +0200

    Fix Unroll_library_function_modifying_incr.invalid.c

--- Unroll_library_function_modifying_incr.invalid.c
+++ Unroll_library_function_modifying_incr.invalid.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 int main() {
     int* x = (int*) malloc(sizeof(int));
     int* y = (int*) malloc(sizeof(int));
     int* z = (int*) malloc(sizeof(int));
     *x = 1;
     *y = 4;
-    for(int i = 0; i < 17; i += (int) x) {
+    for(int i = 0; i < 17; i += *x) {
         memcpy(z, y, 1);
         memcpy(x, z, 1);
         memcpy(y, x, 1);
         printf("a\n");
     }
 }
+
